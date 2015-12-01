df0fb9ba1d57e1aac6a75df950b1b30b4a422dd7
Dan Smith <Dancemyth@icloud.com>
2015-12-01 00:32:57 +0000

    god knows

--- Flags Again/ViewController.swift
+++ Flags Again/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blueFlag: UIImageView!
+    @IBOutlet weak var redFlag: UIImageView!
+    @IBOutlet weak var hideBlueButton: UIButton!
+    @IBOutlet weak var hideRedButton: UIButton!
+    var boolRed: Bool = true
+    var boolBlue: Bool = true
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,21 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func hideRed(sender: AnyObject) {
+        redFlag.hidden = true
+        
+    }
 
 
+    @IBAction func hideBlueFlag(sender: AnyObject) {
+        
+        blueFlag.hidden = true
+    }
+
+    @IBAction func bringThemBack(sender: AnyObject) {
+        blueFlag.hidden = false
+        redFlag.hidden = false
+        
+    }
 }
 
